c03ba6680448e4cab3aa73c27c59e1b7cebcd54e
SJS0001 <58920132+SJS0001@users.noreply.github.com>
2026-03-08 22:31:07 +0100

    Modify SQL query to translate weekdays and format hour

--- SQL/sales_timestamp.sql
+++ SQL/sales_timestamp.sql
@@ -1,9 +1,16 @@
 SELECT
-  FORMAT_DATE('%A', DATE(InvoiceDate)) AS DayOfWeek,
-  EXTRACT(HOUR FROM InvoiceDate) AS Hour,
+  CASE 
+    WHEN DayOfWeek = 'Monday' THEN '1-Pondělí'
+    WHEN DayOfWeek = 'Tuesday' THEN '2-Úterý'
+    WHEN DayOfWeek = 'Wednesday' THEN '3-Středa'
+    WHEN DayOfWeek = 'Thursday' THEN '4-Čtvrtek'
+    WHEN DayOfWeek = 'Friday' THEN '5-Pátek'
+    WHEN DayOfWeek = 'Saturday' THEN '6-Sobota'
+    WHEN DayOfWeek = 'Sunday' THEN '7-Neděle'
+    ELSE DayOfWeek
+  END AS DayOfWeek_Fixed,
+  LPAD(CAST(Hour AS STRING), 2, '0') AS Hour, 
   ROUND(SUM(TotalSum), 2) AS Revenue
 FROM `sqltest-489613.ecommerce_analysis.sales`
-GROUP BY DayOfWeek, Hour
-ORDER BY
-  EXTRACT(DAYOFWEEK FROM InvoiceDate),
-  Hour
+GROUP BY DayOfWeek_Fixed, Hour
+ORDER BY DayOfWeek_Fixed, Hour
