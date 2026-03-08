61b505aced0096800d4acfcadb0b24fa8329e196
SJS0001 <58920132+SJS0001@users.noreply.github.com>
2026-03-08 21:22:03 +0100

    Exclude certain descriptions from top product query
    Added a filter to exclude specific descriptions from the query results.

--- SQL/top_product.sql
+++ SQL/top_product.sql
@@ -3,6 +3,7 @@ SELECT
   SUM(Quantity) AS units_sold,
   ROUND(SUM(TotalSum), 2) AS Revenue
 FROM `sqltest-489613.ecommerce_analysis.sales`
+WHERE Description NOT IN ('POSTAGE', 'DOTCOM POSTAGE', 'MANUAL')
 GROUP BY Description
 ORDER BY Revenue DESC
 LIMIT 10
